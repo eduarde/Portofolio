a695d4b302189d1f014cf34527ff008f6e64c426
Eduard Erja <eduard.erja@gmail.com>
2016-02-02 23:03:49 +0200

    Initial deploy
    - adding website content to repo

---
+++ script/portofolio.js
@@ -0,0 +1,15 @@
+function renderProjects(element){
+	$('.'+ element).load("templates/Projects.html");
+}
+
+function renderHero(element){
+	$('.'+ element).load("templates/Hero.html");
+}
+
+function renderNavigation(element){
+	$('.'+ element).load("templates/Navigation.html");
+}
+
+function renderFooter(element){
+	$('.'+ element).load("templates/Footer.html");
+}
